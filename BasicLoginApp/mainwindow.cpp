b2db8c26ee36911e3c665885266a6ad33a096af8
JamesBagula <72625441+JamesBagula@users.noreply.github.com>
2023-09-23 19:33:03 +0530

    add files

---
+++ BasicLoginApp/mainwindow.cpp
@@ -0,0 +1,34 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_Login_clicked()
+{
+    QString username = ui->lineEdit__username->text();
+    QString password = ui->lineEdit_password->text();
+
+    if (username == "James" && password == "James"){
+        QMessageBox::information(this, "Login", "Username and Password is correct");
+//        hide();
+        secDialog = new SecDialog(this);
+        secDialog->show();
+    }
+    else
+    {
+        QMessageBox::warning(this, "Login", "Username and Password is not correct");
+    }
+}
+
